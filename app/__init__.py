3eabd34bafdd220b42b828e74ec3b92451034eca
Lassi Laiho <lassi.laiho0@gmail.com>
2020-01-14 00:25:36 +0200

    Setup local database access

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,8 @@
-from app.main import app
-from app import views
+from importlib import import_module
+
+from app.main import app, db
+
+import_module("app.main")
+import_module("app.views")
+
+db.create_all()
--- app/main.py
+++ app/main.py
@@ -1,3 +1,8 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///recipe-book.db"
+
+db = SQLAlchemy(app)
