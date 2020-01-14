37ef7f686e2a0418fd5c4bb4b65e4e19228fb82d
Lassi Laiho <lassi.laiho0@gmail.com>
2020-01-14 15:51:14 +0200

    Add Heroku PostgreSQL support

--- app/main.py
+++ app/main.py
@@ -9,7 +9,10 @@ app = Flask(__name__)
 
 bcrypt = Bcrypt(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///recipe-book.db"
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///recipe-book.db"
 
 db = SQLAlchemy(app)
 
