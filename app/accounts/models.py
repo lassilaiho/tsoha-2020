3f5d0351c9e96a5f38da8df924050f6bbfe787bd
Lassi Laiho <lassi.laiho0@gmail.com>
2020-01-18 13:30:35 +0200

    Add summary to index page

--- app/accounts/models.py
+++ app/accounts/models.py
@@ -1,3 +1,5 @@
+from sqlalchemy.sql import text
+
 from app.main import db
 
 
@@ -35,3 +37,20 @@ class Account(db.Model):
 
     def is_authenticated(self):
         return True
+
+    @staticmethod
+    def get_item_and_recipe_counts(account_id):
+        stmt = text("""
+SELECT COUNT(DISTINCT items.id), COUNT(DISTINCT recipes.id)
+FROM accounts
+LEFT JOIN shopping_list_items items ON items.account_id = accounts.id
+LEFT JOIN recipes ON recipes.account_id = accounts.id
+GROUP BY accounts.id
+HAVING accounts.id = :account_id;
+""").params(account_id=account_id)
+        rows = db.session().execute(stmt)
+        try:
+            row = rows.fetchone()
+            return row[0], row[1]
+        finally:
+            rows.close()
--- app/views.py
+++ app/views.py
@@ -1,8 +1,17 @@
-import flask
+from flask import render_template
+from flask_login import current_user, login_required
 
 from app.main import app
+from app.accounts.models import Account
 
 
 @app.route("/")
+@login_required
 def index():
-    return flask.render_template("index.html")
+    item_count, recipe_count = \
+        Account.get_item_and_recipe_counts(current_user.id)
+    return render_template(
+        "index.html",
+        item_count=item_count,
+        recipe_count=recipe_count,
+    )
