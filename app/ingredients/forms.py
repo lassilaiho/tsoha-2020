a28774147e079ca027ceced042cbd84280bcf137
Lassi Laiho <lassi.laiho0@gmail.com>
2020-02-02 12:12:35 +0200

    Tweak number formatting

--- app/ingredients/forms.py
+++ app/ingredients/forms.py
@@ -16,14 +16,19 @@ class RecipeIngredientForm(FlaskForm):
 
     @staticmethod
     def join_amount(value, unit):
-        if value == 1:
-            return unit
+        value = Decimal(value)
+        if value == value.to_integral():
+            value = value.quantize(Decimal(1))
         else:
+            value = value.normalize()
+        if unit:
             return f"{value} {unit}"
+        return str(value)
 
     def parse_amount(self):
         match = amount_pattern.search(self.amount.data)
         if match:
-            return Decimal(match.group("value")), match.group("unit").strip()
+            value = match.group("value").replace(",", ".")
+            return Decimal(value), match.group("unit").strip()
         else:
             return Decimal(1), self.amount.data.strip()
